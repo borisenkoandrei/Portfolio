edfb1821124c5ef6cc3694c06f08610c2b3efea9
Andrey <borisenkoandrei@gmail.com>
2017-03-03 01:21:50 +0300

    0

--- source/js/app.js
+++ source/js/app.js
@@ -1,32 +1,32 @@
-//Parallax
-
-var paralaxContainer = document.querySelector('.parallax'),
-    layers = paralaxContainer.children;
-
-window.addEventListener('mousemove', function (e) {
-
-    var pageX = e.pageX,
-        pageY = e.pageY,
-        initialX = (window.innerWidth / 2) - pageX,
-        initialY = (window.innerHeight / 2) - pageY;
-
-    [].slice.call(layers).forEach(function (layer, i) {
-        var layerStyle = layer.style,
-            divider = i / 40,
-            bottomPosition = (window.innerHeight / 2) * divider,
-            horizontalPosition = (window.innerWidth / 2) * divider,
-            positionX = initialX * divider,
-            positionY = initialY * divider,
-            transformString = 'translate3d(' + positionX + 'px,' + positionY + 'px, 0)';
-
-        layerStyle.transform = transformString;
-        layerStyle.webkitTransform = transformString;
-        layerStyle.bottom = '-' + bottomPosition + 'px';
-        layerStyle.left = '-' + horizontalPosition + 'px';
-        layerStyle.right = '-' + horizontalPosition + 'px';
-    })
-
-});
+// //Parallax
+//
+// var paralaxContainer = document.querySelector('.parallax'),
+//     layers = paralaxContainer.children;
+//
+// window.addEventListener('mousemove', function (e) {
+//
+//     var pageX = e.pageX,
+//         pageY = e.pageY,
+//         initialX = (window.innerWidth / 2) - pageX,
+//         initialY = (window.innerHeight / 2) - pageY;
+//
+//     [].slice.call(layers).forEach(function (layer, i) {
+//         var layerStyle = layer.style,
+//             divider = i / 40,
+//             bottomPosition = (window.innerHeight / 2) * divider,
+//             horizontalPosition = (window.innerWidth / 2) * divider,
+//             positionX = initialX * divider,
+//             positionY = initialY * divider,
+//             transformString = 'translate3d(' + positionX + 'px,' + positionY + 'px, 0)';
+//
+//         layerStyle.transform = transformString;
+//         layerStyle.webkitTransform = transformString;
+//         layerStyle.bottom = '-' + bottomPosition + 'px';
+//         layerStyle.left = '-' + horizontalPosition + 'px';
+//         layerStyle.right = '-' + horizontalPosition + 'px';
+//     })
+//
+// });
 
 //Flipper
 
@@ -42,7 +42,35 @@ html.onclick = function (e) {
 
     if (className == 'authorization'){
         flipper.style.transform = 'rotateY(180deg)';
-    } else{
-        flipper.style.transform = '';
+     } //else{
+    //     flipper.style.transform = '';
+    // }
+}
+
+//blur
+
+var blur = (function () {
+    var wrapper = document.querySelector('.speak'),
+        form = document.querySelector('.feedback__bg');
+
+    return{
+        set: function () {
+            var imgWid = document.querySelector('.speak').offsetWidth,
+                posLeft = -wrapper.offsetLeft,
+                posTop = -wrapper.offsetTop,
+                blurCSS = form.style;
+                console.log(imgWid);
+
+            blurCSS.backgroundSize = imgWid + 'px' + ' ' + 'auto';
+            blurCSS.backgroundPosition = posLeft + 'px' + ' ' + posTop + 'px';
+            
+        }
     }
+}());
+
+blur.set()
+
+window.onresize = function () {
+    blur.set()
+
 }
