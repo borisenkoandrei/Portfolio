2b33ce2d87fb90c871e965132240192dde375f7d
Andrey <boriaenkoandrei@gmail.com>
2017-03-03 15:36:56 +0300

    add mouse parallax

--- source/js/app.js
+++ source/js/app.js
@@ -1,48 +1,81 @@
-//Parallax
+// //Parallax
+//
+// var paralaxContainer = document.querySelector('.parallax'),
+//     layers = paralaxContainer.children;
+//
+// window.addEventListener('mousemove', function (e) {
+//
+//     var pageX = e.pageX,
+//         pageY = e.pageY,
+//         initialX = (window.innerWidth / 2) - pageX,
+//         initialY = (window.innerHeight / 2) - pageY;
+//
+//     [].slice.call(layers).forEach(function (layer, i) {
+//         var layerStyle = layer.style,
+//             divider = i / 40,
+//             bottomPosition = (window.innerHeight / 2) * divider,
+//             horizontalPosition = (window.innerWidth / 2) * divider,
+//             positionX = initialX * divider,
+//             positionY = initialY * divider,
+//             transformString = 'translate3d(' + positionX + 'px,' + positionY + 'px, 0)';
+//
+//         layerStyle.transform = transformString;
+//         layerStyle.webkitTransform = transformString;
+//         layerStyle.bottom = '-' + bottomPosition + 'px';
+//         layerStyle.left = '-' + horizontalPosition + 'px';
+//         layerStyle.right = '-' + horizontalPosition + 'px';
+//     })
+//
+// });
+//
+// //Flipper
+//
+// var authButton = document.querySelector('.authorization'),
+//     flipper = document.querySelector('.flipper'),
+//     html = document.getElementsByTagName('html')[0];
+//
+//
+// html.onclick = function (e) {
+//     var className = e.target.className;
+//
+//     console.log(e.target.className);
+//
+//     if (className == 'authorization'){
+//         flipper.style.transform = 'rotateY(180deg)';
+//     } else{
+//         flipper.style.transform = '';
+//     }
+// };
 
-var paralaxContainer = document.querySelector('.parallax'),
-    layers = paralaxContainer.children;
+//mouseMove
 
-window.addEventListener('mousemove', function (e) {
+var  parallax = (function () {
+    var userBg = document.querySelector('.portfolio-bg__img'),
+        userBgPos = window.getComputedStyle(userBg).top.slice(0, -2),
+        user = document.querySelector('.user'),
+        userPos = window.getComputedStyle(user).top.slice(0, -2),
+        mainBg =document.querySelector('.portfolio-main-bg'),
+        mainBgPos = window.getComputedStyle(mainBg).top.slice(0, -2);
 
-    var pageX = e.pageX,
-        pageY = e.pageY,
-        initialX = (window.innerWidth / 2) - pageX,
-        initialY = (window.innerHeight / 2) - pageY;
-
-    [].slice.call(layers).forEach(function (layer, i) {
-        var layerStyle = layer.style,
-            divider = i / 40,
-            bottomPosition = (window.innerHeight / 2) * divider,
-            horizontalPosition = (window.innerWidth / 2) * divider,
-            positionX = initialX * divider,
-            positionY = initialY * divider,
-            transformString = 'translate3d(' + positionX + 'px,' + positionY + 'px, 0)';
-
-        layerStyle.transform = transformString;
-        layerStyle.webkitTransform = transformString;
-        layerStyle.bottom = '-' + bottomPosition + 'px';
-        layerStyle.left = '-' + horizontalPosition + 'px';
-        layerStyle.right = '-' + horizontalPosition + 'px';
-    })
-
-});
-
-//Flipper
-
-var authButton = document.querySelector('.authorization'),
-    flipper = document.querySelector('.flipper'),
-    html = document.getElementsByTagName('html')[0];
-
-
-html.onclick = function (e) {
-    var className = e.target.className;
-
-    console.log(e.target.className);
-
-    if (className == 'authorization'){
-        flipper.style.transform = 'rotateY(180deg)';
-    } else{
-        flipper.style.transform = '';
+    return{
+        move: function (block, blockPos, windowScroll, strafeAmount) {
+            var strafe = windowScroll * strafeAmount;
+            var style = block.style;
+            style.top = blockPos - strafe + 'px';
+        },
+        init: function (wScroll) {
+            this.move(mainBg, mainBgPos, wScroll, 0.5);
+            this.move(userBg, userBgPos, wScroll, 0.3);
+            this.move(user, userPos, wScroll, 0.2);
+        }
     }
-}
+}());
+
+window.onscroll = function () {
+    var wScroll = window.pageYOffset;
+    console.log(wScroll);
+    parallax.init(wScroll);
+};
+
+
+
